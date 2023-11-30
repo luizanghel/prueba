be858ddd73940091eb6dce1501aefd83ef255cbe
Nico Calbeto Llopart <nico.calbeto@students.salle.url.edu>
2023-11-30 17:06:15 +0100

    Añade la funcion que pide el canal y compruebe que este existe

--- canales.h
+++ canales.h
@@ -1,4 +1,5 @@
 #include "linkedlistprograma.h"
+#include "linkedlistcanales.h"
 
 #ifndef CANALES_H
 #define CANALES_H
@@ -7,5 +8,6 @@ void menuCanales();
 int assignarAlPrograma(int numero, char letra);
 LinkedList4 programaFileToList ();
 void actualizarFicheroPrograma (LinkedList4 programas);
+LinkedList3 canalesFileToList ();
 #endif
 
